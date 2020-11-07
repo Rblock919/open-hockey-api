c7f2e4e730237eae8c297aaf776031083d9f0a17
Ryan Block <ryan.block93@gmail.com>
2020-11-07 12:11:59 -0500

    added a few todo comments

--- src/data-sources/datasources.interface.ts
+++ src/data-sources/datasources.interface.ts
@@ -1,6 +1,7 @@
 import { NHLStatsAPI } from './stats-api.datasource';
 import { NHLRecordsAPI } from './records-api.datasource';
 
+// TODO: figure out if this is still needed
 export interface HockeyDataSources {
   nhlStatsAPI: NHLStatsAPI;
   nhlRecordsAPI: NHLRecordsAPI;
--- src/decorators/datasources.decorator.ts
+++ src/decorators/datasources.decorator.ts
@@ -3,6 +3,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 import { HockeyDataSources } from '../data-sources/datasources.interface';
 
+// TODO: figure out if this is still needed
 export const DataSources = createParamDecorator(
   (data: unknown, context: ExecutionContext): HockeyDataSources => {
     const ctx = GqlExecutionContext.create(context);
